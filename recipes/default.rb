0b2d4754996cd681729cf8c8612a8188d7c94bbd
Florent Flament <contact@florentflament.com>
2015-09-07 18:59:07 +0000

    Remove root password if any

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
 include_recipe 'hostname'
 include_recipe 'chef-dk'
 include_recipe 'users::sysadmins'
+include_recipe 'radicale_box::rootlock'
 
 node['florent_base']['packages'].each do |pk|
   package pk
